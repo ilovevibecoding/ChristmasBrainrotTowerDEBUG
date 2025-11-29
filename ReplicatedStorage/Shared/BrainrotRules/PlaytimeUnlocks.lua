c0f6e9f27a1cd4f2e983044776dd4a062f3c0c21
bart <kuszabartek0@gmail.com>
2025-11-29 18:52:38 +0000

    Updated ReplicatedStorage/Shared/BrainrotRules/PlaytimeUnlocks.lua

---
+++ ReplicatedStorage/Shared/BrainrotRules/PlaytimeUnlocks.lua
@@ -0,0 +1,10 @@
+-- @ScriptType: ModuleScript
+local PlaytimeUnlocks = {
+
+	-- brainrotId = secondsNeeded
+	tung_tung_tung_sahor = 600,   -- unlock after 10 minutes
+	ballerina_cappucina = 1800, -- unlock after 30 minutes
+
+}
+
+return PlaytimeUnlocks
